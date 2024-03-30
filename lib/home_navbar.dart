c32c4ffd1d500469bc829bbcf93cb89044e11fbd
Poojan912 <104124712+Poojan912@users.noreply.github.com>
2024-03-29 23:34:42 -0400

    login and home_navbar

--- lib/home_navbar.dart
+++ lib/home_navbar.dart
@@ -91,3 +91,54 @@ class _CustomNavBarState extends State<CustomNavBar> {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
+//
+// class CustomNavBar extends StatefulWidget implements PreferredSizeWidget {
+//   @override
+//   _CustomNavBarState createState() => _CustomNavBarState();
+//
+//   @override
+//   Size get preferredSize => Size.fromHeight(kToolbarHeight); // Make sure this height fits your needs
+// }
+//
+// class _CustomNavBarState extends State<CustomNavBar> {
+//   final GoogleSignIn _googleSignIn = GoogleSignIn();
+//   bool _isSearchExpanded = false;
+//
+//   Future<void> _handleSignOut() async {
+//     await FirebaseAuth.instance.signOut();
+//     await _googleSignIn.signOut();
+//     // After sign out, navigate to the home page or login screen
+//     Navigator.of(context).pushReplacementNamed('/login'); // Assuming '/login' route leads to the login screen.
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     // ... Rest of your AppBar widget code ...
+//
+//     return AppBar(
+//       // ... Your AppBar properties ...
+//       actions: <Widget>[
+//         // ... Other action widgets ...
+//         PopupMenuButton<int>(
+//           itemBuilder: (context) => [
+//             PopupMenuItem<int>(value: 0, child: Text('Profile')),
+//             PopupMenuItem<int>(value: 1, child: Text('Settings')),
+//             PopupMenuItem<int>(value: 2, child: Text('Logout')),
+//           ],
+//           onSelected: (item) {
+//             switch (item) {
+//               case 2:
+//                 _handleSignOut();
+//                 break;
+//             // Handle other menu items if necessary
+//             }
+//           },
+//         ),
+//       ],
+//     );
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,35 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:http/io_client.dart';
 import 'package:unichat_poojan_project/splash_page.dart';
+import 'firebase_options.dart';
 import 'home_page.dart';
 import 'login_page.dart';
 import 'instructor_login_page.dart';
 import 'school_registration_page.dart';
 
 
-void main() {
 
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:googleapis/drive/v3.dart' as ga;
+import 'package:http/http.dart' as http;
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart';
+
+// void main() {
+//
+//   runApp(MyApp());
+// }
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
@@ -47,12 +69,12 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 4, // Number of tabs
+      length: 3, // Number of tabs
       child: Scaffold(
         appBar: AppBar(
           bottom: TabBar(
             tabs: [
-              Tab(icon: Icon(Icons.home), text: 'Home'),
+              // Tab(icon: Icon(Icons.home), text: 'Home'),
               Tab(icon: Icon(Icons.login), text: 'Login'),
               Tab(icon: Icon(Icons.school), text: 'Instructor Login'),
               Tab(icon: Icon(Icons.app_registration), text: 'School Registration'),
@@ -61,7 +83,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         body: TabBarView(
           children: [
-            HomePage(),
+            // HomePage(),
             LoginScreen(),
             InstructorLoginPage(),
             RegisterSchoolPage(),
