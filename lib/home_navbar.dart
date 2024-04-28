58c12c6bbc9f4394ad3448bc135e3a23f6ac4092
Jainish-dev <jainishnikul2525@gmail.com>
2024-04-28 19:48:52 +0530

    updated profile_url

--- lib/home_navbar.dart
+++ lib/home_navbar.dart
@@ -1,115 +1,122 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:unichat_poojan_project/chat_gpt/screens/gpt_screen.dart';
 import 'package:unichat_poojan_project/main.dart';
 import 'package:unichat_poojan_project/profile_page.dart';
 
-import 'home_page.dart';
-import 'login_page.dart';
+import 'Services/fetchStudentData.dart';
 
 class CustomNavBar extends StatefulWidget implements PreferredSizeWidget {
-  final GoogleSignInAccount? googleUser;
-  CustomNavBar({this.googleUser});
+  final String studentId;
+
+  CustomNavBar({Key? key, required this.studentId}) : super(key: key);
+
   @override
   _CustomNavBarState createState() => _CustomNavBarState();
 
   @override
-  Size get preferredSize => Size.fromHeight(kToolbarHeight); // Make sure this height fits your needs
+  Size get preferredSize => Size.fromHeight(kToolbarHeight); // Customize the AppBar height as needed.
 }
 
 class _CustomNavBarState extends State<CustomNavBar> {
   bool _isSearchExpanded = false;
   final GoogleSignIn googleSignIn = GoogleSignIn(); // GoogleSignIn instance
 
+  Future<Map<String, dynamic>>? studentData;
+
+  @override
+  void initState() {
+    super.initState();
+    studentData = fetchStudentDataAPI().fetchStudentData(widget.studentId);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      backgroundColor: Theme.of(context).primaryColor,
-      elevation: 0,
-      title: _isSearchExpanded
-          ? TextField(
-        autofocus: true,
-        decoration: InputDecoration(
-          hintText: "Search or type a command",
-          border: InputBorder.none,
-          filled: true,
-          fillColor: Colors.white.withOpacity(0.1),
-        ),
-        style: TextStyle(color: Colors.white),
-        onSubmitted: (value) {
-          setState(() {
-            _isSearchExpanded = false; // Optionally collapse after search
-          });
-          // Handle the search operation
-        },
-      )
-          : Text(""),
-      leading: _isSearchExpanded
-          ? BackButton(
-        onPressed: () {
-          setState(() {
-            _isSearchExpanded = false;
-          });
-        },
-      )
-          : null,
-      actions: <Widget>[
-        if (!_isSearchExpanded)
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              setState(() {
-                _isSearchExpanded = true;
-              });
-            },
-          ),
-        if (!_isSearchExpanded)
-          IconButton(
-            icon: Image.asset("assets/ChatGPT_icon.png"), // Assuming it's an SVG. Change it to Image.asset if it's a PNG or JPEG.
-            onPressed: () {
-              // Toggle ChatGPT box
-            },
-          ),
-        if (!_isSearchExpanded)
-          IconButton(
-            icon: Icon(Icons.notifications),
-            onPressed: () {
-              // Handle notifications
-            },
-          ),
-        if (!_isSearchExpanded)
-          IconButton(
-            icon: CircleAvatar(
-              backgroundImage: NetworkImage(widget.googleUser?.photoUrl ?? ''),
-            ),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => ProfilePage(googleUser: widget.googleUser),
-                ),
-              );
-            },
-          ),
+    return FutureBuilder<Map<String, dynamic>>(
+      future: studentData,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return AppBar(title: const Text('Loading...'));
+        } else if (snapshot.hasError) {
+          return AppBar(title: Text("Error: ${snapshot.error}"));
+        }
 
-        if (!_isSearchExpanded)
-          PopupMenuButton<int>(
-              itemBuilder: (context) => [
-                PopupMenuItem<int>(value: 1, child: Text('Settings')),
-                PopupMenuItem<int>(value: 2, child: Text('Logout')),
-              ],
-              onSelected: (item) async {
-                if (item == 2) { // Assuming '2' is the value for Logout
-                  await _handleSignOut();
-                }
-              }
-          ),
-      ],
+        var user = snapshot.data?['studentData'][0];  // Assuming 'studentData' is an array
+
+        return AppBar(
+          backgroundColor: Theme.of(context).primaryColor,
+          elevation: 0,
+          title: Text('${user?['name'] ?? 'User'}'),
+          // leading: _isSearchExpanded ? BackButton(onPressed: _toggleSearch) : null,
+          actions: _buildActions(user),
+        );
+      },
     );
   }
+
+  // TextField _searchTextField() {
+  //   return TextField(
+  //     autofocus: true,
+  //     decoration: InputDecoration(
+  //       hintText: "Search or type a command",
+  //       border: InputBorder.none,
+  //       filled: true,
+  //       fillColor: Colors.white.withOpacity(0.1),
+  //     ),
+  //     style: TextStyle(color: Colors.white),
+  //     onSubmitted: (value) => _toggleSearch(),
+  //   );
+  // }
+
+  List<Widget> _buildActions(Map<String, dynamic>? user) {
+    // if (_isSearchExpanded) return [];
+    return [
+      // IconButton(
+      //   icon: Icon(Icons.search),
+      //   onPressed: _toggleSearch,
+      // ),
+      // IconButton(
+      //   icon: Image.asset("assets/ChatGPT_icon.png"), // Using the ChatGPT icon
+      //   onPressed: () {
+      //     // Implement your action for ChatGPT icon tap here
+      //     debugPrint("ChatGPT icon tapped");
+      //     Navigator.of(context).pushReplacement(MaterialPageRoute(
+      //       builder: (context) => ChatScreen(studentId: widget.studentId), // Navigate to success page
+      //     ));
+      //   },
+      // ),
+      IconButton(
+        icon: CircleAvatar(
+          backgroundImage: user?['photoUrl'] != null
+              ? NetworkImage(user?['photoUrl'])
+              : AssetImage('assets/default_avatar.png') as ImageProvider, // Provide a default avatar if the URL is null
+          radius: 24,
+        ),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => ProfilePage(studentId: widget.studentId)),
+          );
+        },
+      ),
+      PopupMenuButton<int>(
+        itemBuilder: (context) => [
+          PopupMenuItem<int>(value: 1, child: Text('Settings')),
+          PopupMenuItem<int>(value: 2, child: Text('Logout')),
+        ],
+        onSelected: (item) => item == 2 ? _handleSignOut() : null,
+      ),
+    ];
+  }
+
+  // void _toggleSearch() {
+  //   setState(() {
+  //     _isSearchExpanded = !_isSearchExpanded;
+  //   });
+  // }
+
   Future<void> _handleSignOut() async {
-    await googleSignIn.signOut(); // Sign out from Google
-    Navigator.of(context).pushReplacement(
-      MaterialPageRoute(builder: (context) => MyApp()), // Navigate to the LoginPage
-    );
+    await googleSignIn.signOut();
+    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => MyHomePage()));
   }
 }
