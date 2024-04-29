99d313f20833bfba502d60bcb2f67bd1a8f11d21
Jainish-shah <jainishnikul2525@gmail.com>
2024-04-29 01:10:08 -0400

    backend hosting

--- lib/chat_gpt/constants/api_consts.dart
+++ lib/chat_gpt/constants/api_consts.dart
@@ -1,4 +1,4 @@
 String BASE_URL = "https://api.openai.com/v1";
 // String API_Key = "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";
 String API_Key = "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";
-String LOCALHOST = "https://9056-50-75-145-229.ngrok-free.app";
+String LOCALHOST = "https://unichat-caffeinecoders.vercel.app";
